7241d202e819ad435db4ec6315a8411ad75fa28d
Josh Walawender <jmwalawender@gmail.com>
2017-01-24 10:41:05 -1000

    Add instrument_history.py
    Plot the number of nights on the telescope over time.

---
+++ instrument_history.py
@@ -0,0 +1,36 @@
+from __future__ import division, print_function
+
+## Import General Tools
+import sys
+import os
+
+from astropy.table import Table, Column
+
+import matplotlib.pyplot as plt
+
+def main():
+    history_file = 'HIRES_history.csv'
+    sched = Table.read(history_file, format='ascii.csv', guess=False)
+    year = [int(x[0:4]) for x in sched['Date']]
+    sched.add_column(Column(year, name='year'))
+    nightfrac = [1./min([2,len(x.split('/'))]) for x in sched['Instrument']]
+    sched.add_column(Column(nightfrac, name='fraction'))
+    
+    years = []
+    nights = []
+    byyear = sched.group_by('year')
+    for i,val in enumerate(byyear.groups):
+        years.append(byyear.groups[i]['year'][0])
+        nights.append(sum(byyear.groups[i]['fraction']))
+
+    plt.figure(figsize=(16,9))
+    plt.bar(years, nights, width=0.8)
+    plt.xlabel('Year')
+    plt.ylabel('Nights')
+    plt.xlim(1993,2018)
+    plt.grid()
+    plt.savefig('HIRES.png', dpi=72)
+    
+    
+if __name__ == '__main__':
+    main()
