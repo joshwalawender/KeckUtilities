03a0c98dc3a69cf4742807502f88887d03176c9d
Josh Walawender <jmwalawender@gmail.com>
2017-07-30 15:15:51 -1000

    Rewrite to make direct database query.

--- SupportNightCalendar.py
+++ SupportNightCalendar.py
@@ -1,20 +1,16 @@
 #!/usr/env/python
 
-from __future__ import division, print_function
-
 ## Import General Tools
 import sys
 import os
 import argparse
-
-import numpy as np
-from astropy.table import Table
-from astropy import units as u
-from astropy import coordinates as c
-from astropy.time import Time, TimezoneInfo
-from astroplan import Observer
+import requests
 from datetime import datetime as dt
 from datetime import timedelta as tdelta
+import re
+import xml.etree.ElementTree as ET
+
+from astropy.table import Table
 
 class ParseError(Exception):
     def __init__(self, value):
@@ -77,12 +73,106 @@ class ICSFile(object):
             
 
 
+##-------------------------------------------------------------------------
+## Get Telescope Schedule
+##-------------------------------------------------------------------------
+def get_telsched(from_date=None, ndays=10):
+    if from_date is None:
+        now = dt.now()
+        from_date = now.strftime('%y-%m-%d')
+    else:
+        assert dt.strptime(from_date, '%y-%m-%d')
+
+    from_dto = dt.strptime(from_date, '%y-%m-%d')
+    from_string = from_dto.strftime('%Y-%m-%d HST')
+    to_dto = from_dto + tdelta(ndays)
+    to_string = to_dto.strftime('%Y-%m-%d HST')
+    print(f'Getting telescope schedule from {from_string} to {to_string}')
+
+    address = f'http://www/observing/schedule/ws/telsched.php'\
+              f'?date={from_date}&ndays={ndays:d}&field=all&verbosity=-1'
+    r = requests.get(address)
+
+    telsched_xml = ET.fromstring(r.text)
+    keys = ['Date', 'InstrAcc', 'Instrument', 'Location', 'OA', 'Observers',
+            'Principal', 'SA', 'TelNr', 'Twilight']
+    telsched = Table(names=keys,
+                     dtype=('a10', 'a30', 'a30', 'a30', 'a30', 'a50',
+                            'a30', 'a30', 'i4', 'a200'))
+    for i,item in enumerate(telsched_xml.iter('item')):
+        entry = {}
+        for el in item:
+            if el.tag in keys:
+                entry[el.tag] = el.text
+        telsched.add_row(entry)
+    return telsched
+
+
+def parse_twilight(entry):
+    twistring = entry['Twilight']
+    datestr = entry['Date']
+    twilist = twistring.strip('{').strip('}').split(',')
+    twilight = {}
+    for x in twilist:
+        match = re.match("^(\w+)\:'([\w:\-]+)'", x)
+        if match:
+            if match.group(1) == 'sunset':
+                str = '{} {}'.format(entry['Date'], match.group(2))
+                twilight['sunset'] = dt.strptime(str, '%Y-%m-%d %H:%M:%S') +\
+                                     tdelta(1, -10*60*60)
+                twilight['sunsetstr'] = twilight['sunset'].strftime('%H:%M')
+            if match.group(1) == 'sunrise':
+                str = '{} {}'.format(entry['Date'], match.group(2))
+                twilight['sunrise'] = dt.strptime(str, '%Y-%m-%d %H:%M:%S') +\
+                                     tdelta(1, -10*60*60)
+                twilight['sunrisestr'] = twilight['sunrise'].strftime('%H:%M')
+            if match.group(1) == 'dusk_12deg':
+                str = '{} {}'.format(entry['Date'], match.group(2))
+                twilight['dusk_12deg'] = dt.strptime(str, '%Y-%m-%d %H:%M:%S') +\
+                                     tdelta(1, -10*60*60)
+                twilight['dusk_12degstr'] = twilight['dusk_12deg'].strftime('%H:%M')
+            if match.group(1) == 'dawn_12deg':
+                str = '{} {}'.format(entry['Date'], match.group(2))
+                twilight['dawn_12deg'] = dt.strptime(str, '%Y-%m-%d %H:%M:%S') +\
+                                     tdelta(1, -10*60*60)
+                twilight['dawn_12degstr'] = twilight['dawn_12deg'].strftime('%H:%M')
+
+
+    return twilight
+
+##-------------------------------------------------------------------------
+## Check On Call
+##-------------------------------------------------------------------------
+def check_on_call(entry, telsched, args):
+    '''
+    Given an entry (a single row of a telsched table), check to see if the
+    previous night on the same telescope has the same PI string.  If so, the
+    night is "On Call" instead of "Support".
+    '''
+    date = dt.strptime(entry['Date'], '%Y-%m-%d')
+    yesterday = (date-tdelta(1)).strftime('%Y-%m-%d')
+    ysched = telsched[telsched['Date'] == yesterday]
+    yentry = ysched[ysched['TelNr'] == entry['TelNr']]
+    if len(yentry) == 0:
+        type = 'Support'
+    elif len(yentry) == 1:
+        SAmatch = re.search(args.sa.lower(), yentry['SA'][0].lower())
+        PImatch = entry['Principal'] == yentry['Principal'][0]
+        instmatch = entry['Instrument'] == yentry['Instrument'][0]
+        if SAmatch and PImatch and instmatch:
+            type = 'On Call'
+        else:
+            type = 'Support'
+    else:
+        print('Multiple entries for yesterday')
+        type = 'Support'
+    return type
+
 
 ##-------------------------------------------------------------------------
 ## Main Program
 ##-------------------------------------------------------------------------
 def main():
-
     ##-------------------------------------------------------------------------
     ## Parse Command Line Arguments
     ##-------------------------------------------------------------------------
@@ -90,184 +180,57 @@ def main():
     parser = argparse.ArgumentParser(
              description="Program description.")
     ## add arguments
-    parser.add_argument('--sasched',
-        type=str, dest="sasched",  default='sasched.csv',
-        help="The csv file of the SA's duty nights.")
-    parser.add_argument('-t', '--telsched',
-        type=str, dest="telsched", default='telsched.csv',
-        help="The csv file of the telescope schedule.")
-    parser.add_argument('-s', '--start',
-        type=str, dest="start",
-        help="Start time for calendar entry in HHMMSS or HHMM 24 hour format.")
+    parser.add_argument('-s', '--sa',
+        type=str, dest="sa", default='Josh',
+        help="SA name. Use enough of the name to make a case insenstive string search unique.")
     parser.add_argument('-e', '--end',
-        type=str, dest="end", default='2300',
-        help="End time for calendar entry in HHMMSS or HHMM 24 hour format.")
+        type=str, dest="end",
+        help="End date in %Y-%m-%d format or S17B format.")
     args = parser.parse_args()
-
-    if args.start:
-        if len(args.start) == 4:
-            args.start = args.start + '00'
-        elif len(args.start) == 6:
-            args.start = args.start
-        else:
-            raise ParseError('Could not parse start time: "{}"'.format(args.start))
-
+    ## Determine ndays from args.end
     if args.end:
-        if len(args.end) == 4:
-            args.end = args.end + '00'
-        elif len(args.end) == 6:
-            args.end = args.end
+        try:
+            end_dto = dt.strptime(args.end, '%Y-%m-%d')
+        except:
+            pass
         else:
-            raise ParseError('Could not parse end time: "{}"'.format(args.end))
+            today = dt.strptime(dt.now().strftime('%Y-%m-%d'), '%Y-%m-%d')
+            delta = end_dto - today
+            ndays = delta.days
+    else:
+        ndays = 180
 
 
-    ##-------------------------------------------------------------------------
-    ## Read in Telescope Schedule and SA Support Schedule
-    ##-------------------------------------------------------------------------
-    telsched = Table.read(args.telsched, format='ascii.csv', guess=False)
-    assert '#' in telsched.keys()
-    assert 'Date' in telsched.keys()
-    assert 'Instrument' in telsched.keys()
-    assert 'Principal' in telsched.keys()
-    assert 'Observers' in telsched.keys()
-    assert 'Location' in telsched.keys()
-    assert 'InstrAcc' in telsched.keys()
-    sasched = Table.read(args.sasched, format='ascii.csv', guess=False)
-    keys = sasched.keys()
-    assert '#' in keys
-    assert 'Date' in keys
-    assert len(keys) == 3
-    saname = keys.pop(-1)
-
-    sasched = sasched[~sasched[saname].mask]
-    print('Processing {:d} nights in SA schedule.'.format(len(sasched)))
-    type = {'K1': 'Support',
-            'K1T': 'Training',
-            'K1oc': 'On Call',
-            'K2': 'Support',
-            'K2T': 'Training',
-            'K2oc': 'On Call',
-            'K1K2': 'Dual Support',
-            'K1ocK2': 'Dual Support',
-            'K1K2oc': 'Dual Support',
-            'K1ocK2oc': 'Dual Support',
-            }
-
-#     obs = Observer.at_site('Keck')
-    obs = Observer(name='Keck',
-                   longitude=-155.4747185138889, latitude=19.825946547222223,
-                   elevation=4159.581216*u.meter)
-    HST = TimezoneInfo(utc_offset=-10*u.hour, tzname='HST')
-    ## Calculate Elevation of True Horizon from Maunakea
-    ##   Formulas from https://en.wikipedia.org/wiki/Horizon
-    h = 4.2*u.km
-    R = (1.0*u.earthRad).to(u.km)
-    d = np.sqrt(h*(2*R+h))
-    phi = (np.arccos((d/R).value)*u.radian).to(u.deg)
-    MKhorizon = phi - 90*u.deg
-
-    min_before_sunset = 30.
-    min_after_sunrise = 30.
-
+    telsched = get_telsched(ndays=ndays)
+    
     ##-------------------------------------------------------------------------
     ## Create Output iCal File
     ##-------------------------------------------------------------------------
     ical_file = ICSFile('Nights.ics')
 
-    for night in sasched:
-        date = night['Date']
-        telstr = night[saname]
-        print('  {} {}'.format(night['Date'], telstr))
-        time = Time('{} 23:00:00'.format(date))
-
-        sunset = obs.sun_set_time(time, which='next', horizon=MKhorizon)
-        dusk_nauti = obs.sun_set_time(time, which='next', horizon=-12*u.deg)
-        sunrise = obs.sun_rise_time(time, which='next', horizon=MKhorizon)
-        dawn_nauti = obs.sun_rise_time(time, which='next', horizon=-12*u.deg)
-
-#         if obs.moon_altaz(sunset).alt > 0*u.deg:
-#             moon_set = obs.moon_set_time(sunset, which='next',
-#                                          horizon=MKhorizon)
-#             print('Moon Set at {}'.format(moon_set))
-#         else:
-#             moon_rise = obs.moon_rise_time(sunset, which='next',
-#                                            horizon=MKhorizon)
-#             print('Moon Rise at {}'.format(moon_rise))
-
-        if args.start:
-            calend = '{}T{}'.format(date.replace('-', ''), args.start)
-        else:
-            calstart = (sunset.to_datetime(timezone=HST)\
-                        - tdelta(0,min_before_sunset*60.)).strftime('%Y%m%dT%H%M%S')
-        if args.end:
-            calend = '{}T{}'.format(date.replace('-', ''), args.end)
-        else:
-            calend = (sunrise.to_datetime(timezone=HST)\
-                      + tdelta(0,min_after_sunrise*60.)).strftime('%Y%m%dT%H%M%S')
-
-        if telstr != 'K1K2':
-            tel = int(night[saname][1:2])
-            entry = telsched[(telsched['Date'] == date) & (telsched['TelNr'] == tel)]
-            assert len(entry) == 1
-            title = '{} {} ({})'.format(entry['Instrument'][0],
-                                                 type[telstr],
-                                                 entry['Location'][0])
-            description = ['Sunset @ {}'.format(
-                               sunset.to_datetime(timezone=HST).strftime('%H:%M') ),
-                           '12 deg Twilight @ {}'.format(
-                               dusk_nauti.to_datetime(timezone=HST).strftime('%H:%M') ),
-                           '12 deg Twilight @ {}'.format(
-                               dawn_nauti.to_datetime(timezone=HST).strftime('%H:%M') ),
-                           'Sunrise @ {}'.format(
-                               sunrise.to_datetime(timezone=HST).strftime('%H:%M') ),
-                           'PI: {}'.format(entry['Principal'][0]),
-                           'Observers: {}'.format(entry['Observers'][0]),
-                           'Location: {}'.format(entry['Location'][0]),
-                           'Account: {}'.format(entry['InstrAcc'][0]),
+    night_count = 0
+    for entry in telsched:
+        found = re.search(args.sa.lower(), entry['SA'].lower())
+        if found:
+            night_count += 1
+            type = check_on_call(entry, telsched, args)
+            title = '{} {} ({})'.format(entry['Instrument'], type, entry['Location'])
+            twilight = parse_twilight(entry)
+            calend = '{}T{}'.format(entry['Date'].replace('-', ''), '230000')
+            description = [f"Sunset @ {twilight['sunsetstr']}",
+                           f"12 deg Twilight @ {twilight['dusk_12degstr']}",
+                           f"12 deg Twilight @ {twilight['dawn_12degstr']}",
+                           f"Sunrise @ {twilight['sunrisestr']}",
+                           f"PI: {entry['Principal']}",
+                           f"Observers: {entry['Observers']}",
+                           f"Location: {entry['Location']}",
+                           f"Account: {entry['InstrAcc']}",
                            ]
-            ical_file.add_event(title, calstart, calend, description)
-        else:
-            entry = telsched[(telsched['Date'] == date) & (telsched['TelNr'] == 1)]
-            assert len(entry) == 1
-            title = '{} {} ({})'.format(entry['Instrument'][0],
-                                                 type[telstr],
-                                                 entry['Location'][0])
-            description = ['Sunset @ {}'.format(
-                               sunset.to_datetime(timezone=HST).strftime('%H:%M') ),
-                           '12 deg Twilight @ {}'.format(
-                               dusk_nauti.to_datetime(timezone=HST).strftime('%H:%M') ),
-                           '12 deg Twilight @ {}'.format(
-                               dawn_nauti.to_datetime(timezone=HST).strftime('%H:%M') ),
-                           'Sunrise @ {}'.format(
-                               sunrise.to_datetime(timezone=HST).strftime('%H:%M') ),
-                           'PI: {}'.format(entry['Principal'][0]),
-                           'Observers: {}'.format(entry['Observers'][0]),
-                           'Location: {}'.format(entry['Location'][0]),
-                           'Account: {}'.format(entry['InstrAcc'][0]),
-                           ]
-            ical_file.add_event(title, calstart, calend, description)
-
-            entry = telsched[(telsched['Date'] == date) & (telsched['TelNr'] == 2)]
-            assert len(entry) == 1
-            title = '{} {} ({})'.format(entry['Instrument'][0],
-                                                 type[telstr],
-                                                 entry['Location'][0])
-            description = ['Sunset @ {}'.format(
-                               sunset.to_datetime(timezone=HST).strftime('%H:%M') ),
-                           '12 deg Twilight @ {}'.format(
-                               dusk_nauti.to_datetime(timezone=HST).strftime('%H:%M') ),
-                           '12 deg Twilight @ {}'.format(
-                               dawn_nauti.to_datetime(timezone=HST).strftime('%H:%M') ),
-                           'Sunrise @ {}'.format(
-                               sunrise.to_datetime(timezone=HST).strftime('%H:%M') ),
-                           'PI: {}'.format(entry['Principal'][0]),
-                           'Observers: {}'.format(entry['Observers'][0]),
-                           'Location: {}'.format(entry['Location'][0]),
-                           'Account: {}'.format(entry['InstrAcc'][0]),
-                           ]
-            ical_file.add_event(title, calstart, calend, description)
-
+            print(f"{entry['Date']:10s} K{entry['TelNr']:d} {title:s}")
+            ical_file.add_event(title, twilight['sunset'].strftime('%Y%m%dT%H%M%S'),
+                                calend, description)
     ical_file.write()
+    print(f"Found {night_count:d} nights where SA matches {args.sa:}")
 
 
 if __name__ == '__main__':
