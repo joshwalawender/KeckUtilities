b0b0c1fd0388da21e7c398ed43179ef339f54ec6
Josh Walawender <jmwalawender@gmail.com>
2018-08-16 21:23:17 -1000

    Draft general tool for getting VNC sessions

--- get_vnc_sessions.py
+++ get_vnc_sessions.py
@@ -2,21 +2,174 @@
 
 ## Import General Tools
 import sys
-import os
 import re
 import argparse
 import logging
+import yaml
 import paramiko
-from astropy.table import Table
-from socket import gethostname
+from telnetlib import Telnet
 from subprocess import Popen
+from astropy.table import Table, Column
 
 from gooey import Gooey, GooeyParser
+
+
+##-------------------------------------------------------------------------
+## Get Configuration
+##-------------------------------------------------------------------------
+def get_config(filename='keck_vnc_config.yaml'):
+    with open(filename) as FO:
+        config = yaml.load(FO)
+
+    assert 'servers_to_try' in config.keys()
+
+    return config
+
+
+##-------------------------------------------------------------------------
+## Authenticate
+##-------------------------------------------------------------------------
+def authenticate(authpass):
+    config = get_config()
+    assert 'firewall_user' in config.keys()
+    assert 'firewall_address' in config.keys()
+    assert 'firewall_port' in config.keys()
+    firewall_user = config.get('firewall_user')
+    firewall_address = config.get('firewall_address')
+    firewall_port = config.get('firewall_port')
+    with Telnet(firewall_address, int(firewall_port)) as tn:
+        tn.read_until(b"User: ", timeout=5)
+        tn.write(f'{firewall_user}\n'.encode('ascii'))
+        tn.read_until(b"password: ", timeout=5)
+        tn.write(f'{authpass}\n'.encode('ascii'))
+        tn.read_until(b"Enter your choice: ", timeout=5)
+        tn.write('1\n'.encode('ascii'))
+        result = tn.read_all().decode('ascii')
+        if re.search('User authorized for standard services', result):
+            return True
+        else:
+            print(result)
+            return None
+
+
+def close_authentication(authpass):
+    config = get_config()
+    assert 'firewall_user' in config.keys()
+    assert 'firewall_address' in config.keys()
+    assert 'firewall_port' in config.keys()
+    firewall_user = config.get('firewall_user')
+    firewall_address = config.get('firewall_address')
+    firewall_port = config.get('firewall_port')
+    with Telnet(firewall_address, int(firewall_port)) as tn:
+        tn.read_until(b"User: ", timeout=5)
+        tn.write(f'{firewall_user}\n'.encode('ascii'))
+        tn.read_until(b"password: ", timeout=5)
+        tn.write(f'{authpass}\n'.encode('ascii'))
+        tn.read_until(b"Enter your choice: ", timeout=5)
+        tn.write('2\n'.encode('ascii'))
+        result = tn.read_all().decode('ascii')
+        if re.search('User was signed off from all services', result):
+            return True
+        else:
+            print(result)
+            return None
+
+
+##-------------------------------------------------------------------------
+## Determine Instrument
+##-------------------------------------------------------------------------
+def determine_instrument(accountname):
+    accounts = {'mosfire': [f'mosfire{i}' for i in range(1,10)],
+                'hires': [f'hires{i}' for i in range(1,10)],
+                'osiris': [f'osiris{i}' for i in range(1,10)],
+                'lris': [f'lris{i}' for i in range(1,10)],
+                'nires': [f'nires{i}' for i in range(1,10)],
+                'deimos': [f'deimos{i}' for i in range(1,10)],
+                'esi': [f'esi{i}' for i in range(1,10)],
+                'nirc2': [f'nirc{i}' for i in range(1,10)],
+                'nirspec': [f'nirspec{i}' for i in range(1,10)],
+                'kcwi': [f'kcwi{i}' for i in range(1,10)],
+               }
+    accounts['mosfire'].append('moseng')
+    accounts['hires'].append('hireseng')
+    accounts['osiris'].append('osiriseng')
+    accounts['lris'].append('lriseng')
+    accounts['nires'].append('nireseng')
+    accounts['deimos'].append('dmoseng')
+    accounts['esi'].append('esieng')
+    accounts['nirc2'].append('nirceng')
+    accounts['nirspec'].append('nirspeceng')
+    accounts['kcwi'].append('kcwieng')
+
+    for instrument in accounts.keys():
+        if accountname.lower() in accounts[instrument]:
+            return instrument
+
+
+##-------------------------------------------------------------------------
+## Determine VNC Server
+##-------------------------------------------------------------------------
+def determine_VNCserver(accountname, password):
+    config = get_config()
+    servers_to_try = config.get('servers_to_try')
+    vncserver = None
+    for s in servers_to_try:
+        try:
+            print(f'Trying {s}:')
+            client = paramiko.SSHClient()
+            client.load_system_host_keys()
+            client.set_missing_host_key_policy(paramiko.WarningPolicy())
+            client.connect(f"{s}.keck.hawaii.edu", port=22, timeout=6,
+                           username=accountname, password=password)
+            print('  Connected')
+        except TimeoutError:
+            print('  Timeout')
+        except:
+            print('  Failed')
+        else:
+            stdin, stdout, stderr = client.exec_command('kvncinfo -server')
+            rawoutput = stdout.read()
+            vncserver = rawoutput.decode().strip('\n')
+        finally:
+            client.close()
+            if vncserver is not None:
+                print(f"Got VNC server: {vncserver}")
+                break
+    return vncserver
+
+
+##-------------------------------------------------------------------------
+## Determine VNC Sessions
+##-------------------------------------------------------------------------
+def determine_VNC_sessions(accountname, password, vncserver):
+    try:
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.WarningPolicy())
+        client.connect(f"{vncserver}.keck.hawaii.edu", port=22, timeout=6,
+                       username=accountname, password=password)
+        print('  Connected')
+    except TimeoutError:
+        print('  Timeout')
+    except:
+        print('  Failed')
+    else:
+        stdin, stdout, stderr = client.exec_command('kvncstatus')
+        rawoutput = stdout.read()
+        output = rawoutput.decode().strip('\n')
+        allsessions = Table.read(output.split('\n'), format='ascii')
+        sessions = allsessions[allsessions['User'] == accountname]
+        names = [x['Desktop'].split('-')[2] for x in sessions]
+        sessions.add_column(Column(data=names, name=('name')))
+    finally:
+        client.close()
+        return sessions
+
 
 ##-------------------------------------------------------------------------
 ## Main Program
 ##-------------------------------------------------------------------------
-@Gooey
+# @Gooey
 def main():
 
     ##-------------------------------------------------------------------------
@@ -26,14 +179,13 @@ def main():
     parser = GooeyParser(
              description="Get VNC sessions.")
     ## add flags
-    parser.add_argument("-v", "--verbose",
-        action="store_true", dest="verbose",
-        default=False, help="Be verbose! (default = False)")
+    parser.add_argument("-f", "--firewall",
+        dest="firewall",
+        default='', help="Authenticate?")
+    parser.add_argument("-c", "--close",
+        dest="close",
+        default='', help="Close Authentication?")
     ## add arguments
-    parser.add_argument("instrument", type=str,# widget='DropDown',
-        choices=["HIRES", "LRIS", "MOSFIRE", "OSIRIS", "DEIMOS", "ESI", "KCWI",
-                 "NIRES", "NIRSPEC", "NIRC2"],
-        help="The instrument.")
     parser.add_argument("account", type=str,
         help="The user account.")
     parser.add_argument("password", type=str, widget='PasswordField',
@@ -47,51 +199,46 @@ def main():
     log.setLevel(logging.DEBUG)
     ## Set up console output
     LogConsoleHandler = logging.StreamHandler()
-    if args.verbose:
-        LogConsoleHandler.setLevel(logging.DEBUG)
-    else:
-        LogConsoleHandler.setLevel(logging.INFO)
+    LogConsoleHandler.setLevel(logging.DEBUG)
     LogFormat = logging.Formatter('%(asctime)23s %(levelname)8s: %(message)s')
     LogConsoleHandler.setFormatter(LogFormat)
     log.addHandler(LogConsoleHandler)
 
 
     ##-------------------------------------------------------------------------
-    ## Check where this is being run
+    ## Authenticate Through Firewall (or Disconnect)
     ##-------------------------------------------------------------------------
-    if gethostname() == 'Joshs-MBP.local':
-        inKeck = True
-    else:
-        inKeck = False
+    if args.close != '':
+        close_authentication(args.close)
+        return
+    if args.firewall != '':
+        authenticate(args.firewall)
 
 
     ##-------------------------------------------------------------------------
-    ## Determine account
+    ## Determine instrument
     ##-------------------------------------------------------------------------
-    vncServer = {"HIRES": 'svncserver1',
-                 "LRIS": 'svncserver1',
-                 "MOSFIRE": 'svncserver1',
-                 "OSIRIS": 'svncserver1',
-                 "DEIMOS": 'svncserver2',
-                 "ESI": 'svncserver1',
-                 "KCWI": 'vm-kcwi',
-                 "NIRES": 'vm-nires',
-                 "NIRSPEC": 'svncserver2',
-                 "NIRC2": 'svncserver2'}
-    telescope = {'HIRES': 1,
-                 'LRIS': 1,
-                 'MOSFIRE': 1,
-                 'OSIRIS' : 1,
-                 'DEIMOS': 2,
-                 'ESI': 2,
-                 'KCWI': 2,
-                 'NIRES': 2,
-                 'NIRSPEC': 2,
-                 'NIRC2': 2,
-                 }
+    instrument = determine_instrument(args.account)
 
-    log.debug(f'Account: {args.account}')
-    log.debug(f'Instrument: {args.instrument}')
+
+    ##-------------------------------------------------------------------------
+    ## Determine VNC server
+    ##-------------------------------------------------------------------------
+    vncserver = determine_VNCserver(args.account, args.password)
+
+
+    ##-------------------------------------------------------------------------
+    ## Determine VNC Sessions
+    ##-------------------------------------------------------------------------
+    sessions = determine_VNC_sessions(args.account, args.password, vncserver)
+    if len(sessions) == 0:
+        print('No VNC sessions found')
+        return
+
+    for session in sessions:
+        port = int(session['Display'][1:])
+        sshcmd = f"ssh {args.account}@{vncserver}.keck.hawaii.edu -L 59{port:02d}:{vncserver}.keck.hawaii.edu:59{port:02d} -N"
+        print(sshcmd)
 
 
     ##-------------------------------------------------------------------------
@@ -126,48 +273,48 @@ def main():
     ## Get Instrument Sessions
     ##-------------------------------------------------------------------------
 
-    hostname = f"{vncServer[args.instrument]}.keck.hawaii.edu"
-    port = 22
-    command = 'kvncstatus'
-    log.debug(f'Running {command} on {hostname} as {args.account}')
-
-    try:
-        client = paramiko.SSHClient()
-        client.load_system_host_keys()
-        client.set_missing_host_key_policy(paramiko.WarningPolicy())
-        client.connect(hostname, port=port, username=args.account, password=args.password)
-        stdin, stdout, stderr = client.exec_command(command)
-        rawoutput = stdout.read()
-        output = rawoutput.decode()
-    finally:
-        client.close()
-
-    if output == 'No VNC servers found.\n':
-        print(f"No VNC servers found for {args.account}")
-        sys.exit(0)
-
-    tab = Table.read(output, format='ascii')
-    sessions = tab[tab['User'] == args.account]
-
-    for session in sessions:
-        matchname = re.match('(\w+)-(\w+)-(\w+)', session['Desktop'])
-        displayaccount = matchname.group(2)
-        displayname = matchname.group(3)
-
-
-        if inKeck:
-            print(f"# {displayname:<9s}")
-            print(f"open vnc://{hostname}:59{session['Display'][1:]}")
-            if displayname[:7] == 'control':
-                Popen(["open", f"vnc://{hostname}:59{session['Display'][1:]}"])
-        else:
-            outs = [f"# {displayname:<9s}:",
-                    f"/usr/bin/ssh {args.account}@{hostname} -L "+\
-                    f"59{session['Display'][1:]}:{hostname}:59{session['Display'][1:]} -N",
-                    f"open vnc://localhost:59{session['Display'][1:]}",
-                    f""]
-            for line in outs:
-                print(line)
+#     hostname = f"{vncServer[args.instrument]}.keck.hawaii.edu"
+#     port = 22
+#     command = 'kvncstatus'
+#     log.debug(f'Running {command} on {hostname} as {args.account}')
+# 
+#     try:
+#         client = paramiko.SSHClient()
+#         client.load_system_host_keys()
+#         client.set_missing_host_key_policy(paramiko.WarningPolicy())
+#         client.connect(hostname, port=port, username=args.account, password=args.password)
+#         stdin, stdout, stderr = client.exec_command(command)
+#         rawoutput = stdout.read()
+#         output = rawoutput.decode()
+#     finally:
+#         client.close()
+# 
+#     if output == 'No VNC servers found.\n':
+#         print(f"No VNC servers found for {args.account}")
+#         sys.exit(0)
+# 
+#     tab = Table.read(output, format='ascii')
+#     sessions = tab[tab['User'] == args.account]
+# 
+#     for session in sessions:
+#         matchname = re.match('(\w+)-(\w+)-(\w+)', session['Desktop'])
+#         displayaccount = matchname.group(2)
+#         displayname = matchname.group(3)
+# 
+# 
+#         if inKeck:
+#             print(f"# {displayname:<9s}")
+#             print(f"open vnc://{hostname}:59{session['Display'][1:]}")
+#             if displayname[:7] == 'control':
+#                 Popen(["open", f"vnc://{hostname}:59{session['Display'][1:]}"])
+#         else:
+#             outs = [f"# {displayname:<9s}:",
+#                     f"/usr/bin/ssh {args.account}@{hostname} -L "+\
+#                     f"59{session['Display'][1:]}:{hostname}:59{session['Display'][1:]} -N",
+#                     f"open vnc://localhost:59{session['Display'][1:]}",
+#                     f""]
+#             for line in outs:
+#                 print(line)
 
     
 
