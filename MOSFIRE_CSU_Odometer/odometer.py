f2f1395ec95305dbcdc548270afe393485f0d330
Josh Walawender <jmwalawender@gmail.com>
2018-09-24 18:58:03 -1000

    Handle file dates, especially years, properly

--- MOSFIRE_CSU_Odometer/odometer.py
+++ MOSFIRE_CSU_Odometer/odometer.py
@@ -20,49 +20,50 @@ def main():
     nmoves = [0 for i in range(1,93)]
     mileage = [0 for i in range(1,93)]
 
-    log_files = glob('/h/instrlogs/mosfire/*/CSU.log*')
+    log_files = sorted(glob('/h/instrlogs/mosfire/*/CSU.log*'))
     log_files.append('/sdata1300/syslogs/CSU.log')
-    filedates = []
+    filedates = {}
     
-    for log_file in sorted(log_files):
-        year = None
-        folder = os.path.split(os.path.split(log_file)[0])[1]
-        try:
-            if folder[0:2] == '20':
-                year = int(folder[0:4])
-            else:
-                year = int(f'20{folder[0:2]}')
-            assert year > 2000 and year < 2100
-
-        except ValueError:
-            year = dt.utcnow().year
-
+    for i,log_file in enumerate(log_files):
+        month_decimal = 0
+        year_iteration = 0
         last_pos = {}
         for bar in bars:
             last_pos[bar] = 0
         try:
-            print(f'Reading {log_file} (file is for year {year})')
+            print(f'Reading {log_file}')
             with open(log_file, 'r') as FO:
                 contents = FO.readlines()
             print(f'  Analyzing {len(contents):,} lines')
             file_start = None
             file_end = None
+            last_line = ''
             for line in contents:
                 match = re.search(f'Record=<(\d\d),', line)
                 if match is not None:
                     bar = int(match.group(1))
                     md = re.match('(\w+)\s+(\d+)\s+(\d+:\d+:\d+).+', line)
                     if md is not None:
-                        date_string = f"{year} {md.group(1)} {int(md.group(2)):02d} {md.group(3)}"
-                        date = dt.strptime(date_string, '%Y %b %d %H:%M:%S')
+                        date_string = f"{md.group(1)} {int(md.group(2)):02d} {md.group(3)}"
+                        date = dt.strptime(date_string, '%b %d %H:%M:%S')
+
+                        if date.month < month_decimal:
+                            print(f'  Iterating year: {month_decimal} {date.month}')
+                            print(last_line)
+                            print(line)
+                            year_iteration +=1
+                        month_decimal = date.month
+
+
                         if file_start is None:
                             file_start = date
                             ## Look for overlap
-                            for i,fds in enumerate(filedates):
-                                if (file_start >= fds[0]) and (file_start <= fds[1]):
-                                    print(f'WARNING:  Found Overlap with {log_files[i]}')
+                            for compfile in filedates.keys():
+                                fdc = filedates[compfile]
+                                if (file_start >= fdc[0]) and (file_start <= fdc[1]):
+                                    print(f'WARNING:  Found Overlap with {compfile}')
                                     print(file_start)
-                                    print(filedates[i])
+                                    print(compfile, filedates[compfile])
                                     sys.exit(0)
                         else:
                             file_end = date
@@ -73,12 +74,29 @@ def main():
                                 nmoves[bar-1] += 1
                                 mileage[bar-1] += abs(delta)
                         last_pos[bar] = barpos
+                        last_line = line
                     else:
                         print(line)
             assert file_start is not None
             assert file_end is not None
-            filedates.append([file_start, file_end])
-            print(f'  File covers: {file_start} to {file_end}')
+
+            folder = os.path.split(os.path.split(log_file)[0])[1]
+            try:
+                folderdt = dt.strptime(folder, '%Y%m%d')
+            except ValueError:
+                year = dt.utcnow().year
+
+            file_end = dt.strptime(file_end.strftime(f'{folderdt.year} %m %d %H:%M:%S'), '%Y %m %d %H:%M:%S')
+            file_start = dt.strptime(file_start.strftime(f'{folderdt.year-year_iteration} %m %d %H:%M:%S'), '%Y %m %d %H:%M:%S')
+            assert folderdt >= file_end
+
+            timespan = file_end - file_start
+            filedates[log_file] = [file_start, file_end]
+            if i > 0:
+                gap = file_start - filedates[log_files[i-1]][1]
+                print(f'  Gap of {gap} between start of this and end of last log file')
+            print(f'  File covers: {file_start} to {file_end} ({timespan})')
+            
         except UnicodeDecodeError:
             print(f'  Unable to read {log_file}')
 
