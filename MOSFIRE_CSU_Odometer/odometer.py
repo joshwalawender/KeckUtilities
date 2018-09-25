a876021ad7d3c47b969c645866703cd2d0e847fb
Josh Walawender <jmwalawender@gmail.com>
2018-09-24 17:42:32 -1000

    Handle years for overlap checking

--- MOSFIRE_CSU_Odometer/odometer.py
+++ MOSFIRE_CSU_Odometer/odometer.py
@@ -24,12 +24,24 @@ def main():
     log_files.append('/sdata1300/syslogs/CSU.log')
     filedates = []
     
-    for log_file in log_files:
+    for log_file in sorted(log_files):
+        year = None
+        folder = os.path.split(os.path.split(log_file)[0])[1]
+        try:
+            if folder[0:2] == '20':
+                year = int(folder[0:4])
+            else:
+                year = int(f'20{folder[0:2]}')
+            assert year > 2000 and year < 2100
+
+        except ValueError:
+            year = dt.utcnow().year
+
         last_pos = {}
         for bar in bars:
             last_pos[bar] = 0
         try:
-            print(f'Reading {log_file}')
+            print(f'Reading {log_file} (file is for year {year})')
             with open(log_file, 'r') as FO:
                 contents = FO.readlines()
             print(f'  Analyzing {len(contents):,} lines')
@@ -41,14 +53,17 @@ def main():
                     bar = int(match.group(1))
                     md = re.match('(\w+)\s+(\d+)\s+(\d+:\d+:\d+).+', line)
                     if md is not None:
-                        date_string = f"{md.group(1)} {int(md.group(2)):02d} {md.group(3)}"
-                        date = dt.strptime(date_string, '%b %d %H:%M:%S')
+                        date_string = f"{year} {md.group(1)} {int(md.group(2)):02d} {md.group(3)}"
+                        date = dt.strptime(date_string, '%Y %b %d %H:%M:%S')
                         if file_start is None:
                             file_start = date
                             ## Look for overlap
                             for i,fds in enumerate(filedates):
                                 if (file_start > fds[0]) and (file_start < fds[1]):
-                                    print(f'Found Overlap with {log_files[i]}')
+                                    print(f'WARNING:  Found Overlap with {log_files[i]}')
+                                    print(file_start)
+                                    print(filedates[i])
+                                    sys.exit(0)
                         else:
                             file_end = date
                         barpos = float(line.strip('\n').split(',')[2])
@@ -60,7 +75,10 @@ def main():
                         last_pos[bar] = barpos
                     else:
                         print(line)
+            assert file_start is not None
+            assert file_end is not None
             filedates.append([file_start, file_end])
+            print(f'  File covers: {file_start} to {file_end}')
         except UnicodeDecodeError:
             print(f'  Unable to read {log_file}')
 
