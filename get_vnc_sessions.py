8a438710d17dc6c3f3e62f58dc19aa0a4347c91d
Josh Walawender <jmwalawender@gmail.com>
2018-11-26 15:18:10 -1000

    Update default sessions to open

--- get_vnc_sessions.py
+++ get_vnc_sessions.py
@@ -420,13 +420,13 @@ if __name__ == '__main__':
         default=False, action="store_true",
         help="Authenticate through firewall only?")
     parser.add_argument("--control0", dest="control0",
-        default=True, action="store_true",
+        default=False, action="store_true",
         help="Open control0?")
     parser.add_argument("--control1", dest="control1",
-        default=True, action="store_true",
+        default=False, action="store_true",
         help="Open control1?")
     parser.add_argument("--control2", dest="control2",
-        default=True, action="store_true",
+        default=False, action="store_true",
         help="Open control2?")
     parser.add_argument("--telstatus", dest="telstatus",
         default=False, action="store_true",
@@ -470,6 +470,12 @@ if __name__ == '__main__':
         sessions_to_open.append('telanalys')
     if args.status is True:
         sessions_to_open.append('status')
+
+    if len(sessions_to_open) == 0:
+        sessions_to_open.append('control0')
+        sessions_to_open.append('control1')
+        sessions_to_open.append('control2')
+        sessions_to_open.append('telstatus')
 
     config = get_config()
     if 'firewall_address' in config.keys() and\
