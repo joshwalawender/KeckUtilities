0c02c584a5411eb045a6637472c6b8d0c6a365af
Josh Walawender <jmwalawender@gmail.com>
2020-12-02 10:43:51 -1000

    Early draft of CSU fata error analysis

--- MOSFIRE/csu_fatal_errors.py
+++ MOSFIRE/csu_fatal_errors.py
@@ -1,26 +1,263 @@
 #!python3
 
 ## Import General Tools
+import sys
 from pathlib import Path
 import re
 from datetime import datetime
 import subprocess
-from astropy.table import Table, Column
+from astropy.table import Table, Column, Row
 import numpy as np
 
 from matplotlib import pyplot as plt
 
 
 ##-------------------------------------------------------------------------
+## parse_eavesdrop_log
+##-------------------------------------------------------------------------
+def check_for_transition(line, status, pattern, new_state):
+    matched = pattern.match(line)
+    if matched is not None:
+        transition_time = datetime.strptime(matched.group(1), '%Y-%m-%d %H:%M:%S,%f')
+        duration = (transition_time-status[1]).total_seconds()/60
+        dcs1 = get_dcs_keywords(status[1])
+        dcs2 = get_dcs_keywords(transition_time)
+        history_entry = {'status': status[0],
+                         'begin': status[1],
+                         'end': transition_time,
+                         'duration (min)': duration,
+                         'xaccels': -1,
+                         'yaccels': -1,
+                         'accel age (s)': -1,
+                         'ROTPOSN': dcs1['ROTPOSN'],
+                         'ROTPOSN end': dcs2['ROTPOSN'],
+                         'bad': dcs1['bad'],
+                         }
+        return (new_state, transition_time), history_entry
+    else:
+        return status, None
+
+
+def parse_eavesdrop_log(logfile):
+    print(f'Parsing log file: {logfile.name}')
+    try:
+        with open(logfile) as log_file:
+            log_contents = log_file.read()
+            lines = log_contents.split('\n')
+    except:
+        print(f'  Failed to read {logfile}')
+        lines = []
+
+    match_str = ('(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d+) \[mosfire\] DEBUG '
+                 'edu.ucla.astro.irlab.util.Property - Setting property ')
+
+    pxaccel = re.compile(match_str+'<CSUXAccelerometer> to new value <(\d+)>')
+    pyaccel = re.compile(match_str+'<CSUYAccelerometer> to new value <(\d+)>')
+    pstart_setup = re.compile(match_str+'<CSUSetupMaskName> to new value <(\w+)>')
+    pend_setup = re.compile(match_str+'<CSUSetupMaskName> to new value <(\w+)>')
+
+    pstart_move = re.compile(match_str+'<CSUStatus> to new value <Starting group move.>')
+    pend_move = re.compile(match_str+'<CSUStatus> to new value <Performing multiple bar move: 100% complete.>')
+    pfatal_error = re.compile(match_str+'<CSUStatus> to new value <FATAL ERROR >')
+    ppower_down = re.compile(match_str+'<CSUStatus> to new value <Powering down CSU system>')
+    pinitialize = re.compile(match_str+'<CSUStatus> to new value <Bar initialization command sent.>')
+    pinitialize_complete = re.compile(match_str+'<CSUStatus> to new value <Initialization complete.>')
+
+    status_history = list()
+    xaccels = 0
+    yaccels = 0
+
+    status = None
+    for line in lines:
+        if status is None:
+            mstart_time = re.match('(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d+) \[', line)
+            if mstart_time is not None:
+                start_time = datetime.strptime(mstart_time.group(1), '%Y-%m-%d %H:%M:%S,%f')
+#                 print(f'Found start time: {start_time}')
+                status = ('Idle', start_time)
+
+        # Read Xaccel and Yaccel
+        mxaccel = pxaccel.match(line)
+        if mxaccel is not None:
+            xaccel_time = datetime.strptime(mxaccel.group(1), '%Y-%m-%d %H:%M:%S,%f')
+            xaccels = int(mxaccel.group(2))
+        myaccel = pyaccel.match(line)
+        if myaccel is not None:
+            yaccel_time = datetime.strptime(myaccel.group(1), '%Y-%m-%d %H:%M:%S,%f')
+            yaccels = int(myaccel.group(2))
+#             print(f'Accelerations ({status[0]}): {xaccels} {yaccels} ({yaccel_time.strftime("%Y-%m-%d %H:%M:%S")})')
+
+        # Check for Setup start
+        status, history_entry = check_for_transition(line, status, pstart_setup, 'Setup')
+        if history_entry is not None:
+            history_entry['xaccels'] = xaccels
+            history_entry['yaccels'] = yaccels
+            history_entry['accel age (s)'] = (status[1] - min([xaccel_time, xaccel_time])).total_seconds()
+            status_history.append(history_entry)
+
+        # Check for setup end
+        status, history_entry = check_for_transition(line, status, pend_setup, 'Idle')
+        if history_entry is not None:
+            history_entry['xaccels'] = xaccels
+            history_entry['yaccels'] = yaccels
+            history_entry['accel age (s)'] = (status[1] - min([xaccel_time, xaccel_time])).total_seconds()
+            status_history.append(history_entry)
+
+        # Check for move start
+        status, history_entry = check_for_transition(line, status, pstart_move, 'Moving')
+        if history_entry is not None:
+            history_entry['xaccels'] = xaccels
+            history_entry['yaccels'] = yaccels
+            history_entry['accel age (s)'] = (status[1] - min([xaccel_time, xaccel_time])).total_seconds()
+            status_history.append(history_entry)
+
+        # Check for move end
+        status, history_entry = check_for_transition(line, status, pend_move, 'Idle')
+        if history_entry is not None:
+            history_entry['xaccels'] = xaccels
+            history_entry['yaccels'] = yaccels
+            history_entry['accel age (s)'] = (status[1] - min([xaccel_time, xaccel_time])).total_seconds()
+            status_history.append(history_entry)
+
+        # Check for fatal error
+        status, history_entry = check_for_transition(line, status, pfatal_error, 'Error')
+        if history_entry is not None:
+            history_entry['xaccels'] = xaccels
+            history_entry['yaccels'] = yaccels
+            history_entry['accel age (s)'] = (status[1] - min([xaccel_time, xaccel_time])).total_seconds()
+            status_history.append(history_entry)
+
+        # Check for power cycle
+        status, history_entry = check_for_transition(line, status, ppower_down, 'PowerDown')
+        if history_entry is not None:
+            history_entry['xaccels'] = xaccels
+            history_entry['yaccels'] = yaccels
+            history_entry['accel age (s)'] = (status[1] - min([xaccel_time, xaccel_time])).total_seconds()
+            status_history.append(history_entry)
+
+        # Check for initialization
+        status, history_entry = check_for_transition(line, status, pinitialize, 'Initialize')
+        if history_entry is not None:
+            history_entry['xaccels'] = xaccels
+            history_entry['yaccels'] = yaccels
+            history_entry['accel age (s)'] = (status[1] - min([xaccel_time, xaccel_time])).total_seconds()
+            status_history.append(history_entry)
+
+        # Check for initialization complete
+        status, history_entry = check_for_transition(line, status, pinitialize_complete, 'Idle')
+        if history_entry is not None:
+            history_entry['xaccels'] = xaccels
+            history_entry['yaccels'] = yaccels
+            history_entry['accel age (s)'] = (status[1] - min([xaccel_time, xaccel_time])).total_seconds()
+            status_history.append(history_entry)
+
+    return status_history
+
+
+##-------------------------------------------------------------------------
+## get_dcs_keywords
+##-------------------------------------------------------------------------
+def get_dcs_keywords(timestamp):
+    cmd = ['gshow', '-s', 'dcs1',
+           'ROTMODE', 'ROTPOSN', 'EL',
+           '-window', '1s', '-csv',
+           '-date', timestamp.strftime('%Y-%m-%dT%H:%M:%S')]
+    output = subprocess.run(cmd, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+
+    try:
+        contents = output.stdout.decode()
+        dcsdata = Table.read(contents,
+                             format='ascii.csv', comment='#')
+    except:
+        print('Failed to parse result:')
+        print(" ".join(cmd))
+        print(output.stdout.decode())
+        print('-'*40)
+
+    badcol = [ (abs(x['ROTPOSN']) < 10) or (abs(x['ROTPOSN']-180) < 10)
+              or (abs(x['ROTPOSN']+180) < 10)
+              for x in dcsdata]
+    dcsdata.add_column(Column(badcol, name='bad'))
+    year = dcsdata['Date-Time'][0][:4]
+    dcsdata.add_column(Column([year]*len(dcsdata), name='year'))
+    dcsdata.remove_column('Date-Time')
+
+    result = dict()
+    for key in dcsdata.colnames:
+        result[key] = dcsdata[0][key]
+    return result
+
+
+##-------------------------------------------------------------------------
+## get_bar_keywords
+##-------------------------------------------------------------------------
+def get_csu_keywords(timestamp):
+    cmd = ['gshow', '-s', 'mcsus',
+           'B%STAT', 'B%TARG', 'B%POS',
+           '-window', '30s', '-csv',
+           '-date', timestamp.strftime('%Y-%m-%dT%H:%M:%S')]
+    output = subprocess.run(cmd, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    try:
+        contents = output.stdout.decode()
+        csudata = Table.read(contents,
+                             format='ascii.csv', comment='#')
+    except:
+        print('Failed to parse result:')
+        print(output.stdout)
+
+    result = dict()
+    for colname in csudata[-1].colnames:
+        result[colname] = csudata[-1][colname]
+
+    # Get SETUPNAME and MASKNAME
+    cmd = ['gshow', '-s', 'mcsus',
+           'SETUPNAME', 'MASKNAME',
+           '-window', '30s', '-csv',
+           '-date', timestamp.strftime('%Y-%m-%dT%H:%M:%S')]
+    output = subprocess.run(cmd, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    try:
+        contents = output.stdout.decode()
+        namedata = Table.read(contents,
+                              format='ascii.csv', comment='#')
+        result['setupname'] = str(namedata[0]['SETUPNAME'])
+        result['maskname'] = str(namedata[0]['MASKNAME'])
+    except:
+        print('Failed to parse result:')
+        print(" ".join(cmd))
+        print(output.stdout.decode())
+        sys.exit(0)
+        result['setupname'] = ''
+        result['maskname'] = ''
+    
+    return result
+
+
+##-------------------------------------------------------------------------
 ## read_logs_for_fatal_errors
 ##-------------------------------------------------------------------------
-def read_logs_for_fatal_errors():
-    fatal_errors = Table(names=('Date-Time', 'ROTMODE', 'ROTPOSN', 'EL', 'bad', 'year'),
-                         dtype=(np.dtype('S23'), np.dtype('S20'), np.float, np.float, np.bool, np.int))
+def old_read_logs_for_fatal_errors():
+    rotator = Table(names=('Date-Time', 'ROTMODE', 'ROTPOSN', 'EL', 'bad', 'year'),
+                    dtype=(np.dtype('S23'), np.dtype('S20'), np.float, np.float, np.bool, np.int))
+
+    names = ['Date-Time', 'setupname', 'maskname']
+    dtype = [np.dtype('S23'), np.dtype('S23'), np.dtype('S23')]
+    names.extend( [f'B{i:02d}STAT' for i in range(1,93)] )
+    dtype.extend( [np.dtype('S10') for i in range(1,93)] )
+    names.extend( [f'B{i:02d}TARG' for i in range(1,93)] )
+    dtype.extend( [np.float for i in range(1,93)] )
+    names.extend( [f'B{i:02d}POS' for i in range(1,93)] )
+    dtype.extend( [np.float for i in range(1,93)] )
+    csu = Table(names=names, dtype=dtype)
 
     path_eavesdrop = Path('/s/sdata1300/logs/gui/eavesdrop/')
-    match_str = '(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d+) \[mosfire\] DEBUG edu.ucla.astro.irlab.util.Property - Setting property <CSUStatus> to new value <FATAL ERROR >.'
+    match_str = ('(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d+) \[mosfire\] DEBUG '
+                 'edu.ucla.astro.irlab.util.Property - Setting property '
+                 '<CSUStatus> to new value <FATAL ERROR >.')
 
+    table = list()
     years = [15, 16, 17, 18, 19, 20]
     for year in years:
         nlogs = len([x for x in path_eavesdrop.glob(f'{year:02d}*.log')])
@@ -40,46 +277,31 @@ def read_logs_for_fatal_errors():
                 if is_fatal_error is not None:
                     timestamp = datetime.strptime(f'{is_fatal_error.group(1)}000',
                                                   '%Y-%m-%d %H:%M:%S,%f')
-                    cmd = ['gshow', '-s', 'dcs1',
-                           'ROTMODE', 'ROTPOSN', 'EL',
-                           '-window', '1s', '-csv',
-                           '-date', timestamp.strftime('%Y-%m-%dT%H:%M:%S')]
-                    output = subprocess.run(cmd, stdout=subprocess.PIPE,
-                                            stderr=subprocess.PIPE)
+                    print(f'  Fatal error at {timestamp.strftime("%Y-%m-%d %H:%M:%S")}')
+                    csudata = get_csu_keywords(timestamp)
+                    dcsdata = get_dcs_keywords(timestamp)
+                    csudata.update(dcsdata)
+                    table.append(csudata)
 
-                    try:
-                        contents = output.stdout.decode()
-                        dcsdata = Table.read(contents,
-                                             format='ascii.csv', comment='#')
-                        badcol = [ (abs(x['ROTPOSN']) < 10) or (abs(x['ROTPOSN']-180) < 10)
-                                  or (abs(x['ROTPOSN']+180) < 10)
-                                  for x in dcsdata]
-                        dcsdata.add_column(Column(badcol, name='bad'))
-                        dcsdata.add_column(Column([year]*len(dcsdata), name='year'))
-                        fatal_errors.add_row(dcsdata[0])
-                    except:
-                        print('Failed to parse result:')
-                        print(output.stdout)
-
-    return fatal_errors
+    return Table(table)
 
 
-if __name__ == '__main__':
-    csuerror_table_filename = Path('csu_fatal_errors.txt')
-
+def old_main():
+    table_filename = Path('fatal_errors.txt')
     # Read logs and write table file
-    if csuerror_table_filename.exists() is False:
-        fatal_errors = read_logs_for_fatal_errors()
-        fatal_errors.write(csuerror_table_filename, format='ascii.csv')
+    if table_filename.exists() is False:
+        table = read_logs_for_fatal_errors()
+        table.write(table_filename, format='ascii.csv')
 
-    # Make plots
-    fatal_errors = Table.read(csuerror_table_filename, format='ascii.csv')
+    # Make plot of Fatal Errors and Rotator Angle
+    table = Table.read(table_filename, format='ascii.csv')
+    print(table)
+
     plt.figure(figsize=(12,8))
-
-    by_year = fatal_errors.group_by('year')
+    by_year = table.group_by('year')
     colors = {2015: 'k', 2016: 'k', 2017: 'y', 2018: 'b', 2019: 'g', 2020: 'r'}
     for i,yeardata in enumerate(by_year.groups):
-        year = int(f'20{by_year.groups.keys[i][0]}')
+        year = int(f'{by_year.groups.keys[i][0]}')
         plt.plot(yeardata['ROTPOSN'], yeardata['EL'], f'{colors[year]}o',
                  alpha=0.20, label=f'{year} ({len(yeardata)} errors)')
     plt.axvspan(-190,-170, color='r', alpha=0.2)
@@ -94,3 +316,25 @@ if __name__ == '__main__':
     plt.legend(loc='best')
     plt.ylim(-1,91)
     plt.savefig('csu_fatal_errors.png')
+
+
+if __name__ == '__main__':
+    path_eavesdrop = Path('/s/sdata1300/logs/gui/eavesdrop/')
+    fatal_errors = list()
+    status_history = list()
+
+    years = [15, 16, 17, 18, 19, 20]
+    years = [20]
+    for year in years:
+        nlogs = len([x for x in path_eavesdrop.glob(f'{year:02d}*.log')])
+        print(f'Reading {nlogs} logs for 20{year}')
+        for log_eavesdrop in path_eavesdrop.glob(f'{year}*.log'):
+            status = parse_eavesdrop_log(log_eavesdrop)
+            status_history.extend( status )
+
+#     status = parse_eavesdrop_log(path_eavesdrop.joinpath('201129_1803_eavesdrop.log'))
+#     status_history.extend( status )
+
+    history_table = Table(status_history)
+    print(history_table[(history_table['status'] != 'Idle') & (history_table['status'] != 'Moving') & (history_table['status'] != 'Setup')])
+    history_table.write('history_table.txt', format='ascii.fixed_width', overwrite=True)
